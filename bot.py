532996f0915fb56ef337300d9c7f585e6cc449a4
btcraig <btcraig@kalecgos>
2017-07-15 21:34:22 +0000

    yt, got and squad commands working (v1.0)

--- bot.py
+++ bot.py
@@ -5,12 +5,14 @@ from apiclient.discovery import build
 from datetime import date
 from datetime import timedelta
 from threading import Timer
+import pdb
 import dateutil.parser
 import dateutil.relativedelta
 
 with open( './settings.json' ) as f:
     data = json.load( f )
 
+# init global vars...
 client = discord.Client()
 discord_token = data["discord"]["token"]
 youtube_token = data["youtube"]["token"]
@@ -20,6 +22,10 @@ searched = False
 videos = {}
 timer = None
 search_time = datetime.datetime.now() - datetime.timedelta( days=90 )
+search_msg = None
+last_msg = None
+squad = []
+
 
 def write_log( log ):
     logfile = open( data["bot"]["logfile"], "a" )
@@ -32,15 +38,17 @@ def write_log( log ):
     return
 
 def search_helper():
+    """reset search"""
     global searched
     global videos
     global timer
     global search_time
+    global search_msg
     search_time = None
     searched = False
     videos = {}
     timer = None
-    print( "timeout!" )
+    search_msg = None
     return
 
 class yt_video:
@@ -57,8 +65,15 @@ class yt_video:
 
     def set_id( self, i ):
         self.video_id = i
-        
+
+class bee_sting_member():
+    """container for discord member's and time"""
+    def __init__(self, whoami):
+        self.me = whoami
+        self.time = datetime.datetime.now()
+
 def youtube_search( term ):
+    """Search using YouTube's API"""
     global searched
     global timer
     global videos
@@ -103,6 +118,7 @@ def youtube_search( term ):
     return res_str
 
 def get_got_time():
+    """fetch time to next got episode"""
     cur = datetime.datetime.now()
     uri = "http://api.tvmaze.com/shows/82?embed=nextepisode"
     http = urllib3.PoolManager()
@@ -123,34 +139,85 @@ def get_got_time():
     print_str+="until the next Game of Thrones airs!\n\n```"
     return print_str
 
+def the_squad():
+    """fetch squadmate status"""
+    global squad
+    ret_str = ""
+    count = 1
+    ret_str = " \n| Where is The Squad |\n"
+    for m in squad:
+        ret_str += "|  <@"+m.me.id+"> --- "+str(m.me.status)+" --- "+m.time.strftime( "%m/%d %I:%M:%S %p")+"   |\n"
+    ret_str += ""
+    return ret_str
+
+def get_squad():
+    """propogate the squad array"""
+    global squad
+    add = False
+    for u in client.get_all_members():
+        add = False
+        for r in u.roles:
+            if r.name == "the_squad":
+                add = True
+                for s in squad:
+                    if s.me.name == u.name:
+                        add = False
+        if add == True:                    
+            m = bee_sting_member( u )
+            squad.append( m )
+
+@client.event
+async def on_member_join():
+    global squad
+    get_squad()
+
+@client.event
+async def on_member_update( before, after ):
+    global squad
+    for m in squad:
+        if m.me.name == after.name:
+            m.time = datetime.datetime.now()
+            m.me = after
+
 @client.event
 async def on_ready():
+    global squad
     write_log( "bot started!" )
     write_log( "user: "+client.user.name )
     write_log( "id: "+client.user.id )
     write_log( "----------------------" )
+    get_squad()
 
 @client.event
 async def on_message( msg ):
     global searched
+    global search_msg
+    global last_msg
+
     if( msg.author.name.find( me ) >= 0 ):
         return 
 
         # check for prefix
     if( searched == True ):
         cur = datetime.datetime.now()
+        # check if sender matches whomever searched
+        if( search_msg.author.name.find( msg.author.name ) != 0 ):
+            return
+        # check timeout
         if( cur > (search_time + timedelta( seconds=15 ) )):
-            print("timeout")
             search_helper()
             return
+        # check int
         if ( msg.content.isdigit() == False ):
             await client.send_message( msg.channel, "Invalid selection!" )
             return
         val = int( msg.content )
+        # check range
         if( val > 10 or val < 0 ):
             await client.send_message( msg.channel, "Invalid selection!" )
             return
-        await client.send_message( msg.channel, videos[val].title )
+        ret_uri = "https://www.youtube.com/watch?v="+videos[val].video_id
+        last_msg = await client.edit_message( last_msg, ret_uri )
         search_helper()
 
     if( msg.content[:len(prefix)].find( prefix ) >= 0 ):
@@ -158,12 +225,16 @@ async def on_message( msg ):
         args = cmd.split()
         if( args[0] == "yt" ):
             search_str = ""
+            search_msg = msg
+
             for i in args[1:]:
                 search_str+=i+" "
-            await client.send_message( msg.channel, youtube_search( search_str ) )
+            last_msg = await client.send_message( msg.channel, youtube_search( search_str ) )
             return
         elif( args[0] == "got" ):
-            await client.send_message( msg.channel, get_got_time() )
+            last_msg = await client.send_message( msg.channel, get_got_time() )
+        elif( args[0] == "squad" ):
+            last_msg = await client.send_message( msg.channel, the_squad() ) 
         else:
             write_log( "caught unknown cmd" )
             return
