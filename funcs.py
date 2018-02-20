11f18ae676ac9821b43b909ff73c71f873a59c25
btcraig <gnmiller@users.noreply.github.com>
2018-02-20 15:21:55 -0500

    removing deprecated code

--- funcs.py
+++ funcs.py
@@ -14,44 +14,6 @@ def convert( args ):
     c = cc()
     return 
 
-def get_got_time():
-    """Returns a formatted string with the time until the next and time from the previous GoT episode. Episode name is also returned in the string."""
-    now = bs_now()
-    uri = "http://api.tvmaze.com/shows/82?embed[]=nextepisode&embed[]=previousepisode"
-    http = urllib3.PoolManager()
-    response = http.request( 'GET', uri )
-    data = json.loads( response.data.decode( 'utf-8' ) )
-    next_ep = parser.parse( data["_embedded"]["nextepisode"]["airstamp"] ).astimezone( glz() )
-    next_ep_name = data["_embedded"]["nextepisode"]["name"]
-    prev_ep = parser.parse( data["_embedded"]["previousepisode"]["airstamp"] ).astimezone( glz() )
-    prev_ep_name = data["_embedded"]["previousepisode"]["name"]
-    next_diff = relativedelta.relativedelta( next_ep, now )
-    prev_diff = relativedelta.relativedelta( prev_ep, now )
-    ret_str = "```smalltalk\nLast Episode: "+prev_ep_name+" aired "
-    if (abs(prev_diff.months) > 0):
-        ret_str+=str(abs(prev_diff.months))+" months "
-    if (abs(prev_diff.days) > 0):
-        ret_str+=str(abs(prev_diff.days))+" days "
-    if (abs(prev_diff.hours) > 0):
-        ret_str+=str(abs(prev_diff.hours))+" hours "
-    if (abs(prev_diff.minutes) > 0):
-        ret_str+=str(abs(prev_diff.minutes))+" minutes and "
-    if (abs(prev_diff.seconds) > 0):
-        ret_str+=str(abs(prev_diff.seconds))+" seconds ago\n"
-    ret_str+="Next Episode: "+next_ep_name+" airs in "
-    if next_diff.months > 0 :
-        ret_str+=str(next_diff.months)+" months "
-    if next_diff.days > 0 :
-        ret_str+=str(next_diff.days)+" days "
-    if next_diff.hours > 0 :
-        ret_str+=str(next_diff.hours)+" hours "
-    if next_diff.minutes > 0 :
-        ret_str+=str(next_diff.minutes)+" minutes and "
-    if next_diff.seconds > 0 :
-        ret_str+=str(next_diff.seconds)+" seconds\n"
-    ret_str += "```"
-    return ret_str
-
 def magic_8ball():
     """Select and return a random answer from the magic 8-ball"""
     yes = ["It is decidedly so","Without a doubt","Yes, definitely","You can count on it","As I see it: Yes", "Most likely", "Outlook good", "Yes!", "All signs point to yes"]
