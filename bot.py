51cb87f652a44f63d59604c1306797f6613ceb19
btcraig <gnmiller@users.noreply.github.com>
2019-05-16 13:03:49 -0400

    rebuilding bot from the ground up so it doesnt randomly crash and could (ostensibly) be deployed to more than 1 server and definitely work
    as of now youtube search SHOULD be working (as the only command)

--- bot.py
+++ bot.py
@@ -1,140 +1,89 @@
-#!/usr/bin/python3.6
+import discord, asyncio, urllib3, sys, os, logging
+from funcs import *
+##### init #####
+log = setup_logs( 'craig-bot.log' )
+settings = get_settings( 'settings.json' )
+pfx = settings['bot']['prefix']
+discord_token = settings['discord']['token']
+youtube_token = settings['youtube']['token']
+log = setup_logs( 'craig-bot.log' )
 
-import discord, asyncio, pytz, datetime, os, json
-from funcs import get_got_time, magic_8ball, help_str
-from tzlocal import get_localzone as glz
-from server import bs_server
-import pdb
-
-path = os.path.dirname(os.path.realpath(__file__))
-with open( path+'/settings.json' ) as f:
-    settings = json.load( f )
+if not discord.opus.is_loaded():
+    discord.opus.load_opus()
+urllib3.disable_warnings()
 
 client = discord.Client()
-discord_key = settings["discord"]["token"]
-youtube_key = settings["youtube"]["token"]
-tmdb_key = settings["tmdb"]["token"]
-prefix = settings["bot"]["prefix"]
-timeout_val = settings["bot"]["timeout"]
-max_msg = settings["bot"]["stored_msg"]
-date_str = "%m/%d/%y %I:%M %p"
 
-servers = []
+class bt_timer:
+    def __init__(self, timeout, callback, msg):
+        self._timeout = timeout
+        self._callback = callback
+        self._task = asyncio.ensure_future(self._job())
+        self._msg = msg
+
+    async def _job(self):
+        await asyncio.sleep(self._timeout)
+        await self._callback( self._msg )
+
+    def cancel(self):
+        self._task.cancel()
+
+async def bt_timer_cb( msg ):
+    for e in searches:
+        if e[1].id == msg.id:
+            await msg.edit( content='```smalltalk\nTimed out! Please try again!\n```' )
+            try:
+                searches.remove(e)
+            except:
+                pass
+    return
+
+##### bot stuff #####
 @client.event
 async def on_ready():
-    for s in client.servers:
-        servers.append( bs_server( client, s, timeout_val, max_msg ) )
-    await client.change_presence( game=discord.Game( name="indev build" ) )
-    print( "startup finished\n" )
-    
-@client.event
-async def on_server_join( server ):
-    for s in servers:
-        if s == server:
-            return
-    servers.append( bs_server( client, server, timeout_val, max_msg ) )
-    return
+    log.debug( "begin startup process" )
+    path = os.path.dirname( os.path.realpath( __file__ ))
+    with open( path + '/var/bot.pid', mode='w' ) as pid_file:
+        pid_file.write( str(os.getpid()) )
+        pid_file.close()
+    return 0
 
-@client.event
-async def on_member_update( before, after ):
-    for s in servers:
-        for u in s.users:
-            if u == after.id:
-                s.users[u].state = after.status
-                s.users[u].last = datetime.datetime.now().astimezone( glz() )
-    return
-            
+searches = []
 @client.event
 async def on_message( msg ):
-    cur_serv = None
-    for s in servers:
-        if s.server == msg.server:
-            cur_serv = s
-    #ignore PMs/etc
-    if cur_serv == None:
-        return
-    if msg.author == client.user:
-        cur_serv.queue_msg( msg )
-    now = pytz.utc.localize( datetime.datetime.now() )
-    p = msg.content[:len(prefix)]
-    args = msg.content[len(prefix):].split()
-    if p == prefix:
-        args[0] = args[0].lower()
-        cur_serv.queue_cmd( msg )
-        if args[0] == "help" or args[0] == "h":
-            await( client.send_message( msg.author, help_str( prefix ) ))
-        if args[0] == "yt":
-            temp = await cur_serv.search( query_string( args ), "yt", youtube_key )
-            return
-        if args[0] == "tmdb":
-            temp = await cur_serv.search( query_string( args ), "tmdb", tmdb_key )
-            return
-        if args[0] == "whoami":
-            temp = await whoami( cur_serv, msg )
-            return
-        if args[0] == "got":
-            temp = get_got_time()
-            await client.send_message( msg.channel, temp )
-            return
-        if args[0] == "8ball":
-            if len( args ) < 2:
-                await client.send_message( msg.channel, "You need to ask a question!\n" )
+    
+    # check if the message was trying to retrieve a search result and transmit it
+    for e in searches:
+        if msg.author == e[0].author and msg.channel == e[0].channel and msg.guild == e[0].guild:
+            try:
+                int( msg.content )
+            except:
                 return
-            temp = magic_8ball()
-            p_str = "```smalltalk\nYou asked: {}\nThe Magic 8-Ball says: {}```\n".format( msg.content[7:], temp )
-            await client.send_message( msg.channel, p_str )
-            return
-        if args[0] == "status":
-            al = cur_serv.get_auth( msg.author )
-            if al > 3:
-                my_pid = os.getpid()
-                created = os.path.getmtime( "/proc/"+str(my_pid) )
-                creat_str = "```smalltalk\nBot running with PID "+str(my_pid)+" since "
-                creat_str += datetime.datetime.fromtimestamp( int(created) ).strftime( date_str )
-                creat_str += "```\n"
-                await client.send_message( msg.channel, creat_str )
-                return
-            else:
-                await client.send_message( msg.channel, "You are not authorized for this command. Required: 3 ({})\n".format( str( al ) ) )
-                return
-        if args[0] == "set_game":
-            al = cur_serv.get_auth( msg.author )
-            if al < 1:
-                await client.send_message( msg.channel, "You are not authorized.\n" )
-                return
-            if len( args ) < 2:
-                await client.send_message( msg.channel, "You need to specify a message for the game!\n" )
-                return
-            game_str = ""
-            for i in range( 1, len( args ) ):
-                game_str += args[i]+" "
-            game_str = game_str[:len(game_str)-1]
-            await client.change_presence( game=discord.Game( name=game_str ) )
-            await client.send_message( msg.channel, "Setting now playing to: {}\n".format( game_str ) )
-            return
-        return
-    elif cur_serv.busy == True:
-        if cur_serv.mode == "search":
-            if cur_serv.helper.results == None:
-                print( "Results error!\n" )
-            if not msg.content.isdigit():
-                return
-            for i in range( 1, 10 ):
-                if i == int(msg.content):
-                    await cur_serv.get_res( i )
-                    return
-            return
-        
-def query_string( content ):
-    query = ""
-    for i in range( 1, len(content) ):
-        query += content[i]+"+"
-    query = query[:len(query)-1]
-    return query
+            res = yt_search( e[2], youtube_token )
+            video_uri='https://www.youtube.com/watch?v={}'.format(res[int(msg.content)][1])
+            await e[1].edit( content='Selected Option: {}\nTitle: {}\n{}'.format(msg.content, res[int(msg.content)][0], video_uri) )
+            searches.remove(e)
 
-async def whoami( serv, msg ):
-    msg_str = "```smalltalk\nUsername: {}\nID: {}\nAuth Level: {}\n```".format( msg.author.name, msg.author.id, serv.get_auth( msg.author ) )
-    serv.queue_cmd( msg )
-    return await client.send_message( msg.channel, msg_str )
+    # normal query
+    if not chk_pfx( msg.content, pfx ):
+        return
+    else:
+        # strip out the cmd for easier use
+        cmd = msg.content[len(pfx):len(msg.content)]
 
-client.run( discord_key )
+        # youtube search
+        if cmd[0:2] == 'yt':
+            query = cmd[2:len(cmd)]
+            query = query[1:len(query)].lower() 
+            sent = await msg.channel.send( '```smalltalk\nSearching for {}...```'.format( query ) )
+            searches.append( (msg, sent, query) )
+            send_str = '```smalltalk\nPlease select a video:```\n```smalltalk\n'
+            count = 0
+            for e in yt_search( query, youtube_token ) :
+                count+=1
+                send_str += '{}. {}\n'.format( count, e[0] )
+            send_str += '```\n'
+            await sent.edit( content=send_str )
+            # t = bt_timer( 10, bt_timer_cb, sent ) # set a timeout for 10 seconds
+            return
+client.run( discord_token )
--- funcs.py
+++ funcs.py
@@ -1,69 +1,48 @@
-import datetime, urllib3, random, json
-from datetime import date, timedelta
-from dateutil import parser, relativedelta
-from tzlocal import get_localzone
+import json, os, asyncio
 
+def chk_pfx( msg, p ):
+    """check if msg content contains the given prefix"""
+    return msg[:len(p)] == p
 
-def get_got_time():
-    """Returns a formatted string with the time until the next and time from the previous GoT episode. Episode name is also returned in the string."""
-    now = datetime.datetime.now().astimezone( get_localzone() )
-    uri = "http://api.tvmaze.com/shows/82?embed[]=nextepisode&embed[]=previousepisode"
-    http = urllib3.PoolManager()
-    response = http.request( 'GET', uri )
-    data = json.loads( response.data.decode( 'utf-8' ) )
-    next_ep = parser.parse( data["_embedded"]["nextepisode"]["airstamp"] ).astimezone( get_localzone() )
-    next_ep_name = data["_embedded"]["nextepisode"]["name"]
-    prev_ep = parser.parse( data["_embedded"]["previousepisode"]["airstamp"] ).astimezone( get_localzone() )
-    prev_ep_name = data["_embedded"]["previousepisode"]["name"]
-    next_diff = relativedelta.relativedelta( next_ep, now )
-    prev_diff = relativedelta.relativedelta( prev_ep, now )
-    ret_str = "```smalltalk\nLast Episode: "+prev_ep_name+" aired "
-    if (abs(prev_diff.months) > 0):
-        ret_str+=str(abs(prev_diff.months))+" months "
-    if (abs(prev_diff.days) > 0):
-        ret_str+=str(abs(prev_diff.days))+" days "
-    if (abs(prev_diff.hours) > 0):
-        ret_str+=str(abs(prev_diff.hours))+" hours "
-    if (abs(prev_diff.minutes) > 0):
-        ret_str+=str(abs(prev_diff.minutes))+" minutes and "
-    if (abs(prev_diff.seconds) > 0):
-        ret_str+=str(abs(prev_diff.seconds))+" seconds ago\n"
-    ret_str+="Next Episode: "+next_ep_name+" airs in "
-    if next_diff.months > 0 :
-        ret_str+=str(next_diff.months)+" months "
-    if next_diff.days > 0 :
-        ret_str+=str(next_diff.days)+" days "
-    if next_diff.hours > 0 :
-        ret_str+=str(next_diff.hours)+" hours "
-    if next_diff.minutes > 0 :
-        ret_str+=str(next_diff.minutes)+" minutes and "
-    if next_diff.seconds > 0 :
-        ret_str+=str(next_diff.seconds)+" seconds\n"
-    ret_str += "```"
-    return ret_str
+def get_pfx( msg, p ):
+    return msg[:len(p)]
 
-def magic_8ball():
-    """Select and return a random answer from the magic 8-ball"""
-    yes = ["It is decidedly so","Without a doubt","Yes, definitely","You can count on it","As I see it: Yes", "Most likely", "Outlook good", "Yes!", "All signs point to yes"]
-    maybe = ["Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again"]
-    no = ["Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-    r = random.randint( 0, 2 )
-    if r == 0 :
-        return yes[ random.randint( 0, len( yes )-1 ) ]
-    if r == 1 :
-        return maybe[ random.randint( 0, len( maybe )-1 ) ]
-    if r == 2 :
-        return no[ random.randint( 0, len( no )-1 ) ]
-    return "what"
+def get_settings( file ):
+    """Load file as a json and return it"""
+    path = os.path.dirname( os.path.realpath( __file__ ))
+    with open( '{}/{}'.format(path,file) ) as f:
+        settings = json.load( f )
+    f.close()
+    return settings
+settings = get_settings( 'settings.json' )
 
-def help_str( p ):
-    ret_str = "```smalltalk\n{}help/{}h - Display this dialogue.\n    Parameters are given as [optional] and <required>\n\n".format( p, p )
-    ret_str += "{}status - Display the bot's PID and when it was started.\n\n".format( p )
-    ret_str += "{}set_game <message> - Set the bot's \"Now playing\" message.\n\n".format( p )
-    ret_str += "{}whoami - Display's the sender's username, Discord ID and current auth level.\n\n".format( p )
-    ret_str += "{}got - Display when the last GoT episode aired and when the next will air (in terms of days, hours, minutes and seconds).\n\n".format( p )
-    ret_str += "{}yt <search query> - Search YouTube for a video.\n\n".format( p )
-    ret_str += "{}tmdb <search query> - Search TMDb for a film. Currently television show search is not implemented.\n\n".format( p )
-    ret_str += "{}8ball <question> - Ask the magic 8-Ball a question and find out your fate!\n\n".format( p )
-    ret_str += "```"
-    return ret_str
+import logging
+def setup_logs( file ):
+    """Setup logging"""
+    LOG_LEVEL = logging.DEBUG
+    path = '{}/var'.format( os.path.dirname( os.path.realpath( __file__ )) )
+    try:
+        os.mkdir( path )
+    except FileExistsError:
+        # do nothing...
+        pass
+    l = logging.getLogger( 'craig-bot' )
+    l.setLevel( LOG_LEVEL )
+    fh = logging.FileHandler( '{}/bot.log'.format( path ) )
+    fh.setLevel( LOG_LEVEL )
+    f = logging.Formatter( '%(asctime)s - %(name)s - %(levelname)s - %(message)s' )
+    fh.setFormatter(f)
+    l.addHandler( fh )
+    return l
+
+from apiclient.discovery import build
+def yt_search( term, token ):
+    """Return a list contains tuples with (title,id) for YT videos"""
+    yt = build( 'youtube', 'v3', developerKey=token )
+    resp = yt.search().list( q=term, part='id,snippet', maxResults=10 ).execute()
+    result = []
+    for r in resp.get( 'items', [] ):
+        if r['id']['kind'] == 'youtube#video':
+            result.append( (r['snippet']['title'], r['id']['videoId'] ) )
+    return result
+
